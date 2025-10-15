0d54f966ae3c308f21458ce4d74fa8aca984a187
TDF-PL-011 <pkarulis@ZDC-C056.zdc.int>
2025-10-15 18:37:15 +0200

    Alternative model implementation - less false negatives on innitial tests

--- chids/ML/train_model.py
+++ chids/ML/train_model.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
+
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 from keras.layers import Input, Dense
 from keras.models import Model
@@ -10,6 +11,7 @@ from chids.conf.config import *
 from chids.shared.constants import *
 import random
 import tensorflow as tf
+import keras
 
 
 class Training:
@@ -17,19 +19,31 @@ class Training:
     def __init__(self, anomaly_vectors):
         self.anomaly_vectors = anomaly_vectors
 
-
     def _autoencoder_model(self, vectors):
         inputs = Input(shape=(vectors.shape[1], vectors.shape[2]))
 
-        # encoder
-        L1 = Dense(ENCODING_DIM, activation=ACTIVATION, activity_regularizer=regularizers.l1(REG_RATE))(inputs)
-        L2 = Dense(BOTTLENECK, activation=ACTIVATION)(L1)
+        # Enhanced encoder with batch normalization and LeakyReLU
+        L1 = Dense(ENCODING_DIM * 2, activation="linear")(inputs)
+        L1 = keras.layers.LeakyReLU(alpha=0.1)(L1)
+        L1 = keras.layers.BatchNormalization()(L1)
+        L1 = keras.layers.Dropout(0.2)(L1)
 
-        #decoder
-        L3 = Dense(BOTTLENECK, activation=ACTIVATION)(L2)
-        L4 = Dense(DECODING_DIM, activation=ACTIVATION)(L3)
+        L2 = Dense(ENCODING_DIM, activity_regularizer=regularizers.l1(REG_RATE))(L1)
+        L2 = keras.layers.LeakyReLU(alpha=0.1)(L2)
+        L2 = keras.layers.BatchNormalization()(L2)
 
-        output = Dense(vectors.shape[2], activation=ACTIVATION)(L4)
+        L3 = Dense(BOTTLENECK)(L2)
+        L3 = keras.layers.LeakyReLU(alpha=0.1)(L3)
+
+        # Enhanced decoder
+        L4 = Dense(ENCODING_DIM)(L3)
+        L4 = keras.layers.LeakyReLU(alpha=0.1)(L4)
+        L4 = keras.layers.BatchNormalization()(L4)
+
+        L5 = Dense(ENCODING_DIM * 2)(L4)
+        L5 = keras.layers.LeakyReLU(alpha=0.1)(L5)
+
+        output = Dense(vectors.shape[2], activation=ACTIVATION)(L5)
         model = Model(inputs=inputs, outputs=output)
         return model
 
@@ -52,11 +66,20 @@ class Training:
         random.seed(seed)
 
         formatted_anomaly_vectors = _format_input(self.anomaly_vectors)
-        training_anomaly_vectors = formatted_anomaly_vectors.reshape(formatted_anomaly_vectors.shape[0], 1, formatted_anomaly_vectors.shape[1])
+        training_anomaly_vectors = formatted_anomaly_vectors.reshape(
+            formatted_anomaly_vectors.shape[0], 1, formatted_anomaly_vectors.shape[1]
+        )
         model = self._autoencoder_model(training_anomaly_vectors)
         optimizer = optimizers.get(OPTIMIZER)
         loss = losses.get(LOSS_FUNC)
         model.compile(optimizer=optimizer, loss=loss)
-        model.fit(training_anomaly_vectors, training_anomaly_vectors, epochs=EPOCH, batch_size=BATCH_SIZE, validation_split=VALIDATION_SPLIT, verbose=VERBOSE)
+        model.fit(
+            training_anomaly_vectors,
+            training_anomaly_vectors,
+            epochs=EPOCH,
+            batch_size=BATCH_SIZE,
+            validation_split=VALIDATION_SPLIT,
+            verbose=VERBOSE,
+        )
         thresh_list = self._get_thresholds_list(model, training_anomaly_vectors)
         return thresh_list, model
